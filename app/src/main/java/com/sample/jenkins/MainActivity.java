7b40a74743d11ec29040bc36105db313405468c2
Neel Patel <neelpatel6294@gmail.com>
2019-06-05 16:41:07 -0400

    commit 2

--- app/src/main/java/com/sample/jenkins/MainActivity.java
+++ app/src/main/java/com/sample/jenkins/MainActivity.java
@@ -1,13 +1,27 @@
 package com.sample.jenkins;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
